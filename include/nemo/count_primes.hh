76b6970042b3a21b395f9f7ee725b64274db563f
jingleman <41351621+jingleman@users.noreply.github.com>
2021-10-16 08:52:58 -0400

    Initial commit

---
+++ include/nemo/count_primes.hh
@@ -0,0 +1,16 @@
+#ifndef NEMO_COUNT_PRIMES_H_
+#define NEMO_COUNT_PRIMES_H_
+
+#include <iostream>
+
+namespace nemo {
+
+auto est_n_primes(size_t n) -> double;
+
+auto count_primes(size_t n) -> size_t;
+
+auto count_primes_no_goto(size_t n) -> size_t;
+
+} /* end namespace nemo */
+
+#endif
