2cd929de5a55e416243662dc088fb00142be4438
jingleman12 <jingleman12@users.noreply.github.com>
2021-10-23 09:29:36 -0400

    Use refactor tool to move class (def & imp) into header; not perfect: 1. Took a while and froze. 2. Added redundant include(s) & missing include subdir. 3. Didn't recognize need for standalone function wanted (tho caught that as Problem). 4. Included self.

--- src/word_counting.cpp
+++ src/word_counting.cpp
@@ -13,66 +13,8 @@
 #include <vector>
 
 
-bool wanted(char &c) {
-  if (c >= 97 && c <= 122) return true;   // lower case.
-  if (c >= 65 && c <= 90) {c += 32; return true;}  // upper. Could change.
-  if (c == 39 || c == 45)  return true;   // ', -
-  return false;
-}
-
 namespace word_counting {
   
-  std::vector<std::string> load_query(const std::string &path) {
-    
-    std::ifstream ifs(path);
-    if (!ifs) { throw std::invalid_argument("Invalid path " + path); }
-  
-    std::vector<std::string> results;
-    std::string word;
-    while (ifs >> word) {
-      if (word.empty()) { continue; }
-      results.push_back(word);
-    }
-    
-    return results;
-  }
-  
-  struct Database {
-    Database(const Database &other, const std::vector<std::string> &queryKeys) {
-      for (const auto& key : queryKeys) {
-        auto it = other.wc.find(key);
-        if (it == other.wc.end()) {
-          wc[key] = 0;
-        } else {
-          wc[key] = it->second;
-        }
-      }
-    }
-    explicit Database(const std::string& path) {
-      std::ifstream ifs(path);
-      if (!ifs) {throw std::invalid_argument("Invalid path " + path);}
-      std::string word;
-      while (ifs >> word) {
-        if (word.empty()) continue;
-        auto cKept = word.begin();
-        for (auto c: word) {
-          if (wanted(c)) {
-            *cKept++ = c;
-          }
-        }
-        word.erase(cKept, word.end());
-        ++wc[word];
-      }
-    }
-    std::vector<std::pair<std::string, size_t>> wordCounts() const {
-      std::vector<std::pair<std::string, size_t>> results;
-      for (auto pr: wc) results.emplace_back(pr);
-      std::sort(results.begin(), results.end());
-      return results;
-    }
-  private:
-    std::unordered_map<std::string, size_t> wc;
-  };
   auto word_counting(const std::string& corpusPath, const std::string& queryPath) -> void {
     auto corpus = Database(corpusPath);
     std::vector<std::string> queryKeys = load_query(queryPath);
